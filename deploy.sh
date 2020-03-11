1c9796791c009465893b7bcc9e227a862f067de4
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2020-03-11 08:37:39 -0300

    clean up

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,1 @@
-cp -r application deploy
-cp -r layout deploy
-cp index.php deploy
-cp .htaccess deploy
-cp src/fly/flightplan.js deploy
-cp src/database.php deploy/application/config
-cp src/config.php deploy/application/config
-cp src/fly/.gitignore deploy
-
-cd deploy
-git add --a
-git commit -m "fly"
-fly production
+rsync --exclude '.git' -r . nit@nbcgib.uesc.br:/var/www/html/nit/pibiti2020/
